2503cea1dc1a79eeb429c286a8dd78a5c47047a4
Eric Storey <estorey@gmail.com>
2019-03-10 00:11:49 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(array)
- # your code here!
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    nil
+  end
 end
